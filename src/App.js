3c95e9703105a30fe9ab4afc56ffd91f94ef26b6
sinhasudhanshu <108268913+sinhasudhanshu@users.noreply.github.com>
2022-12-03 18:28:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-
+//import Hello from './components/Hello';
+import Sudhanshu from'./components/Sudhanshu';
 function App() {
   return (
-    <center>
-      <h1>Hello React</h1></center>
+    <center>                           
+      <h1>Hello React</h1>
+    <Sudhanshu/>
+    </center>
   );
 }
 
