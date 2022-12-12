b661c09cee264d25be502cea25c6b9b4b602bbef
sinhasudhanshu <108268913+sinhasudhanshu@users.noreply.github.com>
2022-12-12 18:07:43 +0530

    Create VsjItem.js

---
+++ src/components/VsjItem.js
@@ -0,0 +1,10 @@
+function VsjItem(props)
+{
+	const message="Hello Varanasi Software Junction";
+	return <p className="vsj">
+	
+	<h2>{props.message}</h2>
+	<img src=''/>
+	</p>;
+}
+export default VsjItem;
