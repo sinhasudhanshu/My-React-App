c943d73d0439b1264939c4d89c305fe36fb65fa1
sinhasudhanshu <108268913+sinhasudhanshu@users.noreply.github.com>
2022-12-13 15:12:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+// import Add from './components/Add';
 //import Hello from './components/Hello';
 //import Sudhanshu from'./components/Sudhanshu';
-import VsjItem from './components/VsjItem';
+//import VsjItem from './components/VsjItem';
+import Add from './components/Add';
 function App() {
   return (
     <center>                           
       <h1>Hello React</h1>
       <img src='logo192.png'/>
-    <VsjItem message="Hello Sachin"/>
+      
+    <Add/>
     </center>
   );
 }
