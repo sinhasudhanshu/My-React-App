ecf7b264ae3ff9b017a47d5a4a70b2e8f95675ec
sinhasudhanshu <108268913+sinhasudhanshu@users.noreply.github.com>
2022-12-15 15:13:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 //import SimpleInterest from './components/SimpleInterest';
 //import Calculator from './components/Calculator';
-// import Add from './components/Add';
+//import Add from './components/Add';
 //import Hello from './components/Hello';
 //import Sudhanshu from'./components/Sudhanshu';
 //import VsjItem from './components/VsjItem';
@@ -10,13 +10,14 @@ import './App.css';
 //import Subtract from './components/Subtract';
 //import VsjCounter from './components/VsjCounter';
 import MyComponent from './components/MyComponent';
-import VsjHihello from './components/VsjHihello';
+import Addition from './components/Addition';
+//import VsjHihello from './components/VsjHihello';
 function App() {
   return (
     <center>                           
       <h1>Hello React</h1>
       <img src='logo192.png'/>
-      <VsjHihello/>
+      <Addition/>
     <MyComponent/>
     </center>
   );
