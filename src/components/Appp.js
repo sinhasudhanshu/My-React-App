1e424215d5f575d030bac462c825179c0b25773f
sinhasudhanshu <108268913+sinhasudhanshu@users.noreply.github.com>
2022-12-15 13:06:24 +0530

    Create Appp.js

---
+++ src/components/Appp.js
@@ -0,0 +1,14 @@
+import VsjItem from "./VsjItem";
+
+function App(){
+
+    return(
+        <div className="App">
+            <h1>Welcome to VSJ React</h1>
+            <VsjItem message="Hello Varanasi Software Junction"></VsjItem>
+            <VsjItem message="Learn React Here"></VsjItem>
+        </div>
+    );
+}
+
+export default App;
