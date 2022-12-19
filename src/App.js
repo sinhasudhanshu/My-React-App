f1705fc9ef36538a1cf28a5c8f824cfe178aca2a
sinhasudhanshu <108268913+sinhasudhanshu@users.noreply.github.com>
2022-12-19 17:13:57 +0530

    MultipleObject

--- src/App.js
+++ src/App.js
@@ -10,41 +10,23 @@ import { useState } from 'react';
 //import Add from './components/Add';
 //import Subtract from './components/Subtract';
 //import VsjCounter from './components/VsjCounter';
-//import MyComponent from './components/MyComponent';
+import MyComponent from './components/MyComponent';
 //import DropDown from './components/DropDown';
 //import Addition from './components/Addition';
 //import VsjHihello from './components/VsjHihello';
 //import RadioButton from './components/RadioButton';
 //import Checkbox from './components/Checkbox';
 //import VsjA from './components/VsjA';
-import VsjText from './components/VsjText';
+//import VsjText from './components/VsjText';
+import VsjMultipleObject from './components/VsjMultipleObject';
 function App(props) {
-  const [mydata, setMyData] = useState(0);
-  const [mytext, setMyText] = useState(0);
-  const SaveData = (n1) => {    //SaveData me n1 ki value call kr rahe h
+ return(
+     <center>
+  <h2>MultipleObject</h2>
+  <VsjMultipleObject/>
+  <MyComponent/>
 
-    console.log("App");
-
-    setMyData(n1);
-    console.log(mydata);
-
-  };
-  const SaveText = (n2) => {
-
-    console.log("App");
-
-    setMyText(n2);
-    console.log(mytext);
-
-  };
-  return (
-    <div className="App">
-      <h2>From Component A {mydata}</h2>
-      <h2>From Component B {mytext}</h2>
-      <VsjText onSaveData={SaveData} onSaveText={SaveText} /> 
-
-    </div>
-  );
-
+  </center>
+ );
 }
 export default App;
