228ef629681d2fa826648fe90b49e3247ae636c8
sinhasudhanshu <108268913+sinhasudhanshu@users.noreply.github.com>
2022-12-28 16:11:58 +0530

    multiplearray

--- src/App.js
+++ src/App.js
@@ -1,32 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
-//import ArrayTrain from './components/ArrayTrain';
-//import SimpleInterest from './components/SimpleInterest';
-//import Calculator from './components/Calculator';
-//import Sudhanshu from'./components/Sudhanshu';
-//import VsjItem from './components/VsjItem';
-//import Add from './components/Add';
-//import Subtract from './components/Subtract';
-//import VsjCounter from './components/VsjCounter';
-//import MyComponent from './components/MyComponent';
-//import DropDown from './components/DropDown';
-//import Addition from './components/Addition';
-//import VsjHihello from './components/VsjHihello';
-//import RadioButton from './components/RadioButton';
-//import Checkbox from './components/Checkbox';
-//import VsjA from './components/VsjA';
-//import VsjText from './components/VsjText';
-//import VsjMultipleObject from './components/VsjMultipleObject';
-//import ObjectSimpleInterest from './components/ObjectSimpleInterest';
-//import MapUse from './components/MapUse';
-//import MapCities from './components/MapCities';
-import Train from './components/Train';
+
+import React, { useEffect, useState } from 'react';
+import ArrayUpdate from './components/ArrayUpdate';
+//import UpdateArray from './components/UpdateArrayTrain';
+
 function App(props) {
  return(
      <center>
   <h2>MultipleObject</h2>
-   <Train/>
+  <div>
+        <ArrayUpdate/>
+           </div>
+       
   
   </center>
  );
