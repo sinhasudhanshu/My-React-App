1bea02b0507302507f5e6dc822b7cfb8cf67a1a8
sinhasudhanshu <108268913+sinhasudhanshu@users.noreply.github.com>
2022-12-28 16:11:19 +0530

    Update Train.js

--- src/components/Train.js
+++ src/components/Train.js
@@ -1,22 +1,22 @@
 import { useState } from "react"
 import './Train.css';
 const Train = () => {
-    
+
 
     const [trainData, setTrain] = useState([{ trainno: 1253, trainname: "shivganga" }])
     const sumN1 = (event) => {
         console.log(trainData[0].trainno);
         setTrain(
-            
-            [{ trainname: event.target.value,trainno:trainData[0].trainno }]
+
+            [{ trainname: event.target.value, trainno: trainData[0].trainno }]
         );
         console.log("Object Changed");
     };
     const sumN2 = (event) => {
         console.log(trainData[0].trainname);
         setTrain(
-            
-            [{ trainno:event.target.value,trainname:trainData[0].trainname }]
+
+            [{ trainno: event.target.value, trainname: trainData[0].trainname }]
         );
         console.log("Object Changed");
 
@@ -24,9 +24,8 @@ const Train = () => {
     return (
         <div className="myTrain">
             <h2>List of Train</h2>
-            <h2>{trainData[0].trainname }</h2>
-            <h2>{trainData[0].trainno }</h2>
-            
+            <h2>{trainData[0].trainname}</h2>
+            <h2>{trainData[0].trainno}</h2>
             <label>Trai Name</label>
             <input type="text" onChange={sumN1} />
             <label>Train No</label>
@@ -37,6 +36,4 @@ const Train = () => {
         </div>
     )
 };
-
-
 export default Train;
