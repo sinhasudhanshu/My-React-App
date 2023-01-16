224bdefead79fe014ca5363927b20589699aa509
sinhasudhanshu <108268913+sinhasudhanshu@users.noreply.github.com>
2023-01-16 14:28:08 +0530

    MaterialUi Design
    Buttons

--- src/components/MyButtons.js
+++ src/components/MyButtons.js
@@ -1,16 +1,21 @@
 import * as React from 'react';
-import Button from '@mui/material/Button';
 
+import Button from '@mui/material/Button';
 function MyButtons() {
 
     return (
+
         <div>
 
             <Button variant="text">Text</Button>
+
             <Button variant="contained">Contained</Button>
+
             <Button variant="outlined">Outlined</Button>
+
 
         </div>
     );
 }
+
 export default MyButtons;
