56d208c6b90774c343da10916fdb39735ed4b0df
sinhasudhanshu <108268913+sinhasudhanshu@users.noreply.github.com>
2023-01-16 16:31:02 +0530

    CheckBox
    design material ui

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import MyCheckbox from './components/MyCheckbox';
 //import MyButtons from './components/MyButtons';
-import MyButtonsGroup from './components/MyButtonsGroup';
+//import MyButtonsGroup from './components/MyButtonsGroup';
 
 function App() {
     return (<div>
 
         <center>
-        <MyButtonsGroup />
+       <MyCheckbox/>
         
         </center>
 
