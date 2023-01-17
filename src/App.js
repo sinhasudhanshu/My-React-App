f7be10c3135651d717f3f8b21a44a8da5ddbdda2
sinhasudhanshu <108268913+sinhasudhanshu@users.noreply.github.com>
2023-01-17 13:36:06 +0530

    radio button

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 //import MyCheckbox from './components/MyCheckbox';
 //import MyCheckboxColor from './components/MyCheckboxColor';
 //import MyFloatingButton from './components/MyFloatingButton';
-import MyRadioButton from './components/MyRadioButton';
+//import MyRadioButton from './components/MyRadioButton';
+import Ratingbutton from './components/RatingButton';
 //import MyButtons from './components/MyButtons';
 //import MyButtonsGroup from './components/MyButtonsGroup';
 
@@ -12,7 +13,8 @@ function App() {
 
 
         <center>
-       <MyRadioButton/>
+       
+       <Ratingbutton/>
         
         </center>
 
---
+++ src/components/RatingButton.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import Rating from '@mui/material/Rating';
+import Stack from '@mui/material/Stack';
+function Ratingbutton() {
+
+    return (
+        <div>
+
+            <Rating name="half-rating" defaultValue={2.5} precision={0.5} />
+            <Rating name="half-rating-read" defaultValue={2.5} precision={0.5} readOnly />
+        </div>
+    );
+}
+export default Ratingbutton;
