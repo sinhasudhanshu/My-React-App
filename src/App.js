a88a56e6186dbbbdd83c7b113c7d02cac87105b2
sinhasudhanshu <108268913+sinhasudhanshu@users.noreply.github.com>
2023-01-20 21:47:05 +0530

    Popper Material UI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import PopoverBasic from './components/PopoverBasic';
+// import PopoverBasic from './components/PopoverBasic';
 // import StackBasic from './components/StackBasic';
 // import StackDivider from './components/StackDivider';
 // import StandardImageList from './components/StandarImageList';
 // import WovenImageList from './components/WovenImageList';
-
+import PopperBasic from './components/PopperBasic';
+import PopperPositioned from './components/PopperPositioned';
 
 function App() {
     return (<div>
@@ -13,7 +14,9 @@ function App() {
 
         <center>
 
-          <PopoverBasic/>
+          <PopperBasic/>
+          <br></br><br></br><br></br>
+          <PopperPositioned/>
         
         </center>
 
