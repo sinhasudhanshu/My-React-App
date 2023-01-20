efc24c627cbaa3a0d6566d9d20921972d975d17f
sinhasudhanshu <108268913+sinhasudhanshu@users.noreply.github.com>
2023-01-20 16:14:23 +0530

    Menu Material UI

--- src/App.js
+++ src/App.js
@@ -1,64 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 
-//import NestedLists from './components/NestedLists';
-//import InteractiveLists from './components/IntereactiveLists';
-//import MyCheckbox from './components/MyCheckbox';
-//import MyCheckboxColor from './components/MyCheckboxColor';
-//import MyFloatingButton from './components/MyFloatingButton';
-//import MyRadioButton from './components/MyRadioButton';
-//import Ratingbutton from './components/RatingButton';
-//import SelectButton from './components/SelectButton';
-//import SelectButtons from './components/SelectButtons';
-//import MultipleSelectNative from './components/MultipleSelectNative';
-//import SliderButton from './components/SliderButton';
-//import SliderVolumeButton from './components/SliderVolumeButton';
-//import BasicSwitchButton from './components/BasicSwitchButton';
-//import SwitchColor from './components/Switch
-//import TextFields from './components/TextFields';
-//import TransferLists from './components/TransferLists';
-//import TransferList from './components/TransferList';
-//import IconAvatars from './components/IconAvatars';
-//import LetterAvatars from './components/LetterAvatars';
-//import BasicBadge from './components/BasicBadge';
-//import BadgeVisibility from './components/BadgeVisibility';
-// import ChipClickButton from './components/ChipClickButton';
-// import ChipDeleteButton from './components/ChipDeleteButton';
-// import InsetDividers from './components/InsetDividers';
-// import MaterialsIcon from './components/MeterialsIcon';
-// import ColorIcon from './components/ColorIcon';
-//import MyButtons from './components/MyButtons';
-//import MyButtonsGroup from './components/MyButtonsGroup';
-//import BasicTable from './components/BasicTable';
-//import DataTablee from './components/DataTablee';
-// import BasicTooltip from './components/BasicTooltip';
-// import PositionedTooltips from './components/PositionedTooltip';
-// import CustomizedTooltips from './components/CustomizedTooltip';
-// import TypoGraaphFunction from './components/TypoGraaphFunction';
-// import TypographyTheme from './components/TypegraphTheme';
-// import BasicAlerts from './components/BasicAlerts';
-// import DescriptionAlerts from './components/DescriptionAlerts';
-//import SimpleBackdrop from './components/SimpleBackDrop';
-// import BasicDialog from './components/BasicDialog';
-// import FormDialog from './components/FormDialog';
-// import FullscreenDialog from './components/FullscreenDialog';
-// import ProgressCircular from './components/ProgressCircular';
-// import ProgressInterective from './components/ProgressInterective';
-// import ProgressStatic from './components/ProgressStatic';
-// import SkeletonYouTube from './components/SkeletonYoutube';
-// import SkeletonFacebook from './components/SkeletonFacebook';
-// import AccordionBasic from './components/AccordionBasic';
-// import AccordionControlled from './components/AccordionControlled';
-// import AccordionCustomized from './components/AccordionCustomized';
-// import BasicCard from './components/BasicCard';
-// import OutlineCard from './components/OutlineCard';
-// import SimpleBottomNavigation from './components/SimpleBottomNavigation';
-// import LabelBottomNavigation from './components/LabelBottomNavigation';
-// import BasicBreadcrumbs from './components/BasicBreadcrums';
-// import CustomBreadcrums from './components/CustomBreadcrums';
-// import SimplePaper from './components/SimplePaper';
-// import TemporaryDrawer from './components/TemporaryDrawer';
-import Links from './components/Links';
+// import Links from './components/Links';
+import IconMenu from './components/IconMenu';
 
 
 
@@ -68,7 +12,7 @@ function App() {
 
         <center>
          
-        < Links/>
+        <IconMenu/>
         </center>
 
     </div>
