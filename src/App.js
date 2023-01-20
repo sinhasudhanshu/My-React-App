51444343217bdadc364f57006cb68ff5fd717c92
sinhasudhanshu <108268913+sinhasudhanshu@users.noreply.github.com>
2023-01-20 20:56:38 +0530

    Stacks Material UI

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-
-// import Links from './components/Links';
-// import IconMenu from './components/IconMenu';
-// import DenseMenu from './components/DenseMenu';
-// import PaginationOutlined from './components/PaginationOutlined';
-// import PaginationBasic from './components/PaginationBasic';
-// import StepperLinear from './components/StepperLinear';
-// import StepperNonLinear from './components/TabsBasic';
-// import BasicTabs from './components/TabsBasic';
-// import ScrollableTabs from './components/ScrollableTabs';
-// import BoxSx from './components/BoxSx';
-// import SimpleContainer from './components/SimpleContainer';
-import GridBasic from './components/GridBasic';
-import GridSpacing from './components/GridSpacing';
+import StackBasic from './components/StackBasic';
+import StackDivider from './components/StackDivider';
 
 function App() {
     return (<div>
 
 
         <center>
-        <GridBasic/>
-        <br></br>
-        <tr></tr>
-        <GridSpacing/>
+
+            <StackBasic/>
+            <br></br><br></br><br></br><br></br><br></br>
+            <StackDivider/>
         
         </center>
 
